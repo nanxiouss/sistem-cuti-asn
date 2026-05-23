de73115dc62ceb846cd6f01ef30308ac34e49c90
Shin Hakuro <muhammadrizky040206@gmail.com>
2026-05-23 15:47:11 +0700

    pembuatan role kasubbag_umum di seeder

--- database/seeders/PegawaiSeeder.php
+++ database/seeders/PegawaiSeeder.php
@@ -16,10 +16,14 @@ class PegawaiSeeder extends Seeder
         // 1. Cari user berdasarkan NIP yang sudah dibuat di UserSeeder
         $kadin = User::where('nip', '11111111')->first();
         $sekdin = User::where('nip', '12345678')->first();
+        $kasubbag_umum = User::where('nip', '55555555')->first();
         $kabid = User::where('nip', '22222222')->first();
+        $kabid1 = User::where('nip', '21212121')->first();
         $kasi = User::where('nip', '33333333')->first();
+        $kasi1 = User::where('nip', '31313131')->first();
         $admin = User::where('nip', '99999999')->first();
         $pegawai = User::where('nip', '44444444')->first();
+        $pegawai1 = User::where('nip', '41414141')->first();
 
         // Pastikan datanya ketemu sebelum membuat Pegawai
         if($kadin && $sekdin && $kabid && $kasi && $admin && $pegawai) {
@@ -46,6 +50,17 @@ class PegawaiSeeder extends Seeder
                 'sisa_cuti_tahunan' => 12
             ]);
 
+            // Kasubbag_umum (Atasan: Admin)
+            Pegawai::create([
+                'user_id' => $kasubbag_umum->id,
+                'atasan_id' => $sekdin->id,
+                'pangkat_golongan' => 'Pembina / IV.a',
+                'jabatan' => 'Kepala Subbag Umum & Kepegawaian',
+                'unit_kerja' => 'Subbag Umum & Kepegawaian',
+                'tmt_kerja' => '2014-09-19',
+                'sisa_cuti_tahunan' => 12
+            ]);
+
             // Kabid (Atasan: Kadin)
             Pegawai::create([
                 'user_id' => $kabid->id,
@@ -54,6 +69,16 @@ class PegawaiSeeder extends Seeder
                 'jabatan' => 'Kepala Bidang Ketenagalistrikan',
                 'unit_kerja' => 'Bidang Ketenagalistrikan',
                 'tmt_kerja' => '2017-05-22',
+                'sisa_cuti_tahunan' => 12
+            ]);
+
+            Pegawai::create([
+                'user_id' => $kabid1->id,
+                'atasan_id' => $kadin->id,
+                'pangkat_golongan' => 'Pembina / IV.a',
+                'jabatan' => 'Kepala Bidang UPTD LAB',
+                'unit_kerja' => 'Bidang UPTD LAB',
+                'tmt_kerja' => '2016-03-15',
                 'sisa_cuti_tahunan' => 12
             ]);
 
@@ -68,10 +93,20 @@ class PegawaiSeeder extends Seeder
                 'sisa_cuti_tahunan' => 12
             ]);
 
-            // Admin (Tidak kita set atasannya)
+            Pegawai::create([
+                'user_id' => $kasi1->id,
+                'atasan_id' => $kabid->id,
+                'pangkat_golongan' => 'Penata / III.c',
+                'jabatan' => 'Kepala Subbag Tata Usaha',
+                'unit_kerja' => 'Subbag Tata Usaha',
+                'tmt_kerja' => '2016-03-21',
+                'sisa_cuti_tahunan' => 12
+            ]);
+
+            // Admin (Atasan: Kasubbag_umum)
             Pegawai::create([
                 'user_id' => $admin->id,
-                'atasan_id' => null,
+                'atasan_id' => $kasubbag_umum->id,
                 'pangkat_golongan' => 'Penata Muda / III.a',
                 'jabatan' => 'Analis Kepegawaian',
                 'unit_kerja' => 'Subbag Umum dan Kepegawaian',
@@ -88,6 +123,17 @@ class PegawaiSeeder extends Seeder
                 'unit_kerja' => 'Seksi Analisis',
                 'tmt_kerja' => '2020-04-21',
                 'no_telepon' => '0895645465498',
+                'sisa_cuti_tahunan' => 12
+            ]);
+
+            Pegawai::create([
+                'user_id' => $pegawai1->id,
+                'atasan_id' => $kasi->id,
+                'pangkat_golongan' => 'Pengatur Tingkat I / II.d',
+                'jabatan' => 'Staf Tata Usaha',
+                'unit_kerja' => 'Subbag Tata Usaha',
+                'tmt_kerja' => '2019-05-19',
+                'no_telepon' => '0895642355498',
                 'sisa_cuti_tahunan' => 12
             ]);
         }
