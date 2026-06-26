07844e460e6e1737ca0681aa8a46daf6ce777dbc
Shin Hakuro <muhammadrizky040206@gmail.com>
2026-06-26 17:47:04 +0700

    revisi beta 3

--- resources/views/components/layouts/kabid/navigation.blade.php
+++ resources/views/components/layouts/kabid/navigation.blade.php
@@ -32,13 +32,6 @@
             </div>
 
             <div class="flex items-center gap-3 sm:gap-4">
-                <button class="relative p-2.5 text-slate-400 hover:text-lime-600 hover:bg-lime-50 rounded-full transition-all duration-200 focus:outline-none">
-                    <span class="absolute top-2.5 right-2.5 w-2 h-2 bg-rose-500 rounded-full ring-2 ring-white"></span>
-                    <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path>
-                    </svg>
-                </button>
-
                 <div class="h-6 w-px bg-slate-200 mx-1 hidden sm:block"></div>
 
                 <div class="relative" @click.outside="profileOpen = false">
